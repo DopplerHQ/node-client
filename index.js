3219f21af2e1f6c7eba2826789d6f18deba8837d
Brian Vallelunga <vallelungabrian@gmail.com>
2019-05-09 00:56:29 -0700

    updated indentation

--- index.js
+++ index.js
@@ -6,20 +6,20 @@ const config = require("./package")
 
 
 class Doppler {
-  
+
   constructor(data) {
-    if(data.api_key === null || data.api_key.length === 0) {
+    if (data.api_key === null || data.api_key.length === 0) {
       throw new Error("Please provide an 'api_key' on initialization.")
     }
-    
-    if(data.environment === null || data.environment.length === 0) {
+
+    if (data.environment === null || data.environment.length === 0) {
       throw new Error("Please provide a 'environment' on initialization.")
     }
-    
-    if(data.pipeline === null) {
+
+    if (data.pipeline === null) {
       throw new Error("Please provide a 'pipeline' on initialization.")
     }
-    
+
     this.pipeline = data.pipeline
     this.environment = data.environment
     this.remote_keys = {}
@@ -31,14 +31,14 @@ class Doppler {
       "client-version": data.client_version || config.version,
       "client-sdk": data.client_sdk || "node.js"
     }
-    
-    if(data.backup_filepath) {
-      this.backup_filepath = path.resolve(process.cwd(), data.backup_filepath) 
+
+    if (data.backup_filepath) {
+      this.backup_filepath = path.resolve(process.cwd(), data.backup_filepath)
     }
-    
+
     this.startup()
   }
-  
+
   // Private Methods
   startup(retry_count = 0) {
     const _this = this
@@ -51,79 +51,87 @@ class Doppler {
       },
       path: "/v1/variables"
     })
-    
+
     const success = response[0]
     const body = response[1]
-    
-    if(success) {
+
+    if (success) {
       _this.remote_keys = body.variables
       _this.override_keys()
       _this.write_env()
       return
     }
-    
-    if(!body || !body.messages) {
-      if(retry_count < _this.max_retries) {
+
+    if (!body || !body.messages) {
+      if (retry_count < _this.max_retries) {
         retry_count += 1
         return _this.startup(retry_count)
       } else {
-        if(!!_this.backup_filepath && fs.existsSync(_this.backup_filepath)) {
+        if (!!_this.backup_filepath && fs.existsSync(_this.backup_filepath)) {
           const response = dotenv.parse(fs.readFileSync(_this.backup_filepath))
-          
-          if(response !== null) {
+
+          if (response !== null) {
             console.log("DOPPLER: Falling back to local backup at " + _this.backup_filepath)
             _this.remote_keys = response
             _this.override_keys()
             return
           }
         }
-        
-        console.error("DOPPLER: Failed to reach Doppler servers after " + retry_count  + " retries...")
+
+        console.error("DOPPLER: Failed to reach Doppler servers after " + retry_count + " retries...")
       }
     }
-    
+
     _this.error_handler(body)
   }
-  
+
   write_env() {
-    if(!this.backup_filepath) { return }
-    
+    if (!this.backup_filepath) {
+      return
+    }
+
     var remote_body = []
-    
-    for(var key in this.remote_keys) {
-      if(!this.remote_keys.hasOwnProperty(key)) { continue }
-      
+
+    for (var key in this.remote_keys) {
+      if (!this.remote_keys.hasOwnProperty(key)) {
+        continue
+      }
+
       const value = this.remote_keys[key]
       remote_body.push(key + " = " + value)
     }
-    
+
     fs.writeFile(this.backup_filepath, remote_body.join("\n"), function(error) {
-      if(error !== null) {
+      if (error !== null) {
         console.error("Failed to write backup to disk with path " + this.backup_filepath)
-      }  
+      }
     })
   }
-  
-    
+
+
   // Public Methods
-  
+
   // Override: Custom ignore method  
-  get(key_name) { 
+  get(key_name) {
     return this.remote_keys[key_name]
   }
-  
-  override_keys() {    
+
+  override_keys() {
     var override_keys = Object.keys(this.remote_keys)
-    
-    for(var i in override_keys) {
-      if(!override_keys.hasOwnProperty(i)) { continue }
-      
+
+    for (var i in override_keys) {
+      if (!override_keys.hasOwnProperty(i)) {
+        continue
+      }
+
       const key_name = override_keys[i]
-      if(this.ignore_variables.has(key_name)) { continue }
+      if (this.ignore_variables.has(key_name)) {
+        continue
+      }
       process.env[key_name] = this.remote_keys[key_name]
     }
   }
-  
+
   request(data) {
     try {
       const res = requestSync(data.method, (this.host + data.path), {
@@ -132,37 +140,41 @@ class Doppler {
         headers: this.request_headers,
         timeout: 1500
       })
-      
+
       return [
         (res.statusCode === 200),
         JSON.parse(res.body.toString("utf8"))
       ]
-    
+
     } catch (error) {
-      return [ false, null ]
+      return [false, null]
     }
   }
-  
+
   error_handler(response) {
-    if(!response || !response.messages) { return }
-    
+    if (!response || !response.messages) {
+      return
+    }
+
     response.messages.forEach(function(error) {
       console.error(error)
     })
   }
-  
+
 }
 
 var sharedInstance = null;
 module.exports = function(data = {}) {
-  if(sharedInstance != null) { return sharedInstance }
-  
+  if (sharedInstance != null) {
+    return sharedInstance
+  }
+
   const env_path = data.env_filepath || ".env"
   const env = fs.existsSync(env_path) ? dotenv.parse(fs.readFileSync(env_path)) : {}
 
   data.api_key = data.api_key || process.env.DOPPLER_API_KEY || env.DOPPLER_API_KEY || null
   data.pipeline = data.pipeline || process.env.DOPPLER_PIPELINE || env.DOPPLER_PIPELINE || null
   data.environment = data.environment || process.env.DOPPLER_ENVIRONMENT || env.DOPPLER_ENVIRONMENT || null
-    
+
   return sharedInstance = new Doppler(data)
 }
